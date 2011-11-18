5b7d00e16b21ae8f5e1aa3cac0e745f153203a9a
Rich Dammkoehler <rpd@noradltd.com>
2011-11-18 15:26:40 -0600

    Rich D & Chris Powers & Kristin Smith: makin it really cool now

--- lib/retrier.rb
+++ lib/retrier.rb
@@ -1,22 +1,48 @@
 
 module Scratch
   module Retrier
-    def try attempts, options = {}
-      success_blk = options[:success]
-      error_blk = options[:error]
-      waiter = options[:waiter]
-      wait_time = options[:wait_time]
-      wait_blk = options[:wait_blk]
-      attempts.times do
-        begin
-          yield
-          success_blk.call if success_blk
-          break
-        rescue Exception => e
-          error_blk.call(e) if error_blk
-          waiter.wait(wait_time) { wait_blk.call if wait_blk } if waiter
+    class RetryBuilder
+      def initialize attempts, block_proc
+        @attempts = attempts
+        @block_proc = block_proc
+        @wait_count = 0
+      end
+      def success &success_blk
+        @success_blk = success_blk
+        return self
+      end
+      def error &error_blk
+        @error_blk = error_blk
+        return self
+      end
+      def wait minutes, interval = 1, &wait_blk
+        @wait_minutes = minutes
+        @wait_interval = interval
+        @wait_blk = wait_blk
+        return self
+      end
+      def go
+        @attempts.times do
+          begin
+            @block_proc.call
+            @success_blk.call if @success_blk
+            break
+          rescue Exception => e
+            @error_blk.call(e) if @error_blk
+            if @wait_blk
+              sleep (60 * @wait_minutes)
+              @wait_count += 1
+              if @wait_count % @wait_interval == 0
+                @wait_blk.call @wait_count 
+              end
+            end
+          end
         end
       end
+    end
+
+    def try attempts, &block
+      RetryBuilder.new attempts, block
     end
   end
 end
--- spec/retrier_spec.rb
+++ spec/retrier_spec.rb
@@ -43,16 +43,15 @@ describe "retrier" do
   it "runs the given block" do
     runs = 0
     
-    retrier.try(1, { :success => success, :error => error, :waiter => waiter, :wait_time => wait_time, :wait_blk => wait_blk }) {
+    retrier.try(1) {
       runs += 1
-    }
+    }.go
     
     runs.should eq 1
   end
   
   it "runs on_success if things worked out" do
-    retrier.try(5, { :success => success, :error => error, :waiter => waiter, :wait_time => wait_time, :wait_blk => wait_blk }) {
-    }
+    retrier.try(5) {}.success { success.call }.go
     
     @success_ct.should eq 1
   end
@@ -60,29 +59,25 @@ describe "retrier" do
   it "only needs a try count" do
     runs = 0
 
-    retrier.try(1) { runs += 1 }
+    retrier.try(1) { runs += 1 }.go
 
     runs.should eq 1
   end
 
   it "runs on_error if things didn't work out" do
-    retrier.try(5, { :success => success, :error => error, :waiter => waiter, :wait_time => wait_time, :wait_blk => wait_blk }) {
+    retrier.try(5) {
       raise "blah"
-    }
+    }.error { |e| error.call e }.go
 
     @error_ct.should eq 5
   end
 
   it "uses waiter if things didn't work out" do
-    retrier.try(5, { 
-                  :success => success, 
-                  :error => error, 
-                  :waiter => waiter, 
-                  :wait_time => wait_time, 
-                  :wait_blk => wait_blk 
-                }) {
+    retrier.try(5) {
       raise "blah"
-    }
+    }.wait(0) { |wc|
+      wait_blk.call wc
+    }.go
 
     @wait_ct.should eq 5
   end
@@ -90,15 +85,11 @@ describe "retrier" do
   it "error_blk recieves the Exception from the try" do
     ex = "fooie"
     received_ex = ""
-    retrier.try(2, { 
-                  :success => success, 
-                  :error => proc { |e| received_ex = e }, 
-                  :waiter => waiter, 
-                  :wait_time => wait_time, 
-                  :wait_blk => wait_blk }) {
+    retrier.try(2) {
       raise ex
+    }.error { |e|
+      received_e.message.should eq ex
     } 
-    received_ex.message.should eq ex
   end
 
 end
